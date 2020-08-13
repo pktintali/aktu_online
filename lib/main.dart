1c6cb2921b5c790d68dded38bd2fe28f29a67bb6
Pradeep Tintali <47480465+pktintali@users.noreply.github.com>
2020-08-13 06:52:22 +0530

    Updated Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: #FFEBEE,
+      backgroundColor: Color(0xFFFFEBEE),
       body: Stack(
         children: <Widget>[
           Container(
